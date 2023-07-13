a59107f8916cf34dfecc652c6ff4563ccef36832
Ry <rgarn3@gmail.com>
2023-07-13 12:50:30 -0400

    New react app
    Created a new repository and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary</header>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
